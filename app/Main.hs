94024cb9522f641d635164c00f76ca4fb7071926
kamoii <>
2019-09-08 06:09:05 +0900

    [WIP] Add sample

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,51 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import Prelude()
+import qualified Data.Vault.Lazy as V
+import Relude hiding (get)
+import System.Environment (getEnv)
+import Web.Scotty
+import qualified Network.Wai.Middleware.LoginWithTwitter as LWT
+import qualified Network.Wai as W
+
+-- | Set http://localhost:8080/login-with-twitter as valid callbacl url
 main :: IO ()
-main = pure ()
+main = do
+  (consumerKey, consumerSecret) <- readConsumerVals
+  (vkey, lwtMiddleware) <- LWT.middleware (mkLWTConfig consumerKey consumerSecret)
+
+  scotty 8080 $ do
+    middleware lwtMiddleware
+
+    get "/" do
+      pure ()
+
+    post "/login-with-twitter" do
+      req <- request
+      case V.lookup vkey (W.vault req) of
+        Just (LWT.Success user) ->
+          pure ()
+        Just _ ->
+          pure ()
+        Nothing ->
+          pure ()
+
+  where
+    readConsumerVals = do
+      consumerKey <- getEnv "TWITTER_CONSUMER_KEY"
+      consumerSecret <- getEnv "TWITTER_CONSUMER_SECRET"
+      when (consumerKey == "" || consumerSecret == "") do
+        putStrLn "Set TWITTER_CONSUMER_KEY and TWITTER_CONSUMER_SECRET environment variables."
+        exitFailure
+      pure (consumerKey, consumerSecret)
+
+    mkLWTConfig consumerKey consumerSecret = LWT.Config
+      { LWT.configOrigin = "http://localhost:8080"
+      , LWT.configLoginPath = "/login-with-twitter"
+      , LWT.configCallbackPath = "/login-with-twitter"
+      , LWT.configConsumerKey = encodeUtf8 consumerKey
+      , LWT.configConsumerSecret = encodeUtf8 consumerSecret
+      }
